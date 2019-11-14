fa3dc163b5836f7926f31f7ee3f995b637d43712
sing22 <d.singh.3@elsevier.com>
2019-11-14 12:39:00 +0000

    Updated to loop through dir and putting automation to automatically pick all .j2 files and create .tf

--- jinja2_template_load.py
+++ jinja2_template_load.py
@@ -2,8 +2,7 @@
 #
 # Using the file system load
 #
-# We now assume we have a file in the same dir as this one called
-# test_template.html
+# We now assume we have a file in the same dir as this one called `*.j2`
 #
 
 from jinja2 import Environment, FileSystemLoader
@@ -17,13 +16,13 @@ TABLEAU_VERSION="v1"
 def print_html_doc():
     # Create the jinja2 environment.
     # Notice the use of trim_blocks, which greatly helps control whitespace.
-    j2_env = Environment(loader=FileSystemLoader(THIS_DIR),
-                         trim_blocks=True)
-    j2_env.get_template('ec2.tf.j2').stream( version=TABLEAU_VERSION).dump("terraform/ec2-generated.tf")
-
-   # print j2_env.get_template('ec2.tf.j2').render(
-    #    version='dapinder_100'
-    #)
+    for filename in os.listdir(THIS_DIR):
+        if filename.endswith(".j2"):
+            j2_env = Environment(loader=FileSystemLoader(THIS_DIR),
+                                 trim_blocks=True)
+            file_name_with_no_ext = filename[:-6] #removing last 6 chars i.e. .tf.j2
+            generated_file_name = file_name_with_no_ext + "-generated.tf"
+            j2_env.get_template(filename).stream( version=TABLEAU_VERSION).dump("terraform/"+generated_file_name)
 
 if __name__ == '__main__':
     print_html_doc()
